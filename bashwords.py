477b99b9ca98340504b18b2ce069f64569618fc5
jamesob <james.obeirne@gmail.com>
2009-09-05 16:52:41 -0400

    adding actual code

---
+++ bashwords.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+import optparse
+import os
+import datetime
+import cPickle
+import random
+
+# -------------------
+# globals
+# -------------------
+
+home = os.getenv("HOME")
+
+# -------------------
+# actions
+# -------------------
+
+def define():
+    """Define the current word according to the env variable."""
+    word    = os.getenv("currWord")
+    defin   = os.getenv("currDefinition")
+    syns    = os.getenv("currSynonyms")
+
+    print("\t%s\n\tDefinition: %s\n\tSynonyms: %s"
+          % (word, defin, syns))
+
+def add():
+    """
+    Crack open the dictionary with cPickle, add the word, then
+    dump it back to file.
+    """
+    dict = loadDict()
+
+    word = raw_input("Which word would you like to add?: ")
+    defin = raw_input("Its definition?: ")
+    syns = raw_input("Some synonyms? (delimit each with ','): ").split(',') 
+
+    newEntry = {"word": word,
+                "definition": defin,
+                "synonyms": syns,
+                "hits": 0,
+                "dateInserted": datetime.date.today()}
+
+    dict.append(newEntry)
+    numEntries = len(dict)
+
+    dumpDict(dict)
+
+    print("Word added. %d total words in collection." % numEntries)
+
+def delete():
+	"""Open the dictionary, delete an entry, dump modified dictionary."""
+	dict = loadDict()
+
+	mark = raw_input("Word to delete: ")
+
+	index = -1
+
+	for i in dict:
+		if i["word"] == mark:
+			index = dict.index(i)
+	
+	if index == -1:
+		print("'%s' not found. No modifications made." % mark)
+	else:
+		del dict[index]
+		print("Entry for '%s' successfully removed." % mark)
+
+	dumpDict(dict)
+
+def cycle():
+	"""
+	Unpickles dictionary, selects a random word, loads relevant
+	information into BASH's environment.
+	"""
+	dict = loadDict()
+
+	currWord = random.sample(dict, 1)[0]
+
+	with open(home + '/.bashwords/exports.sh', 'w') as f:
+		f.write('export currWord="%s"\n' % currWord["word"])
+		f.write('export currDefinition="%s"\n' % currWord["definition"])
+		f.write('export currSynonyms="%s"\n' % currWord["synonyms"])
+		f.write('PS1="$currWord|$PS1"\n')     
+
+# -------------------
+# utilities
+# -------------------
+
+def loadDict():
+    """Crack the dictionary open, return it."""
+
+    with open(home + "/.bashwords/thewords.dat", "r") as f:
+        dict = cPickle.load(f)
+
+    return dict
+
+def dumpDict(newDict):
+    """Serialize the dictionary back out."""
+    
+    with open(home + "/.bashwords/thewords.dat", "w") as f:
+        cPickle.dump(newDict, f)
+
+    return True
+    
+if __name__ == '__main__':
+	import sys
+	choice = sys.argv[1]
+
+	{'add':     add,
+     'define':  define,
+	 'cycle':   cycle}[choice]()
+
+
---
+++ configure.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+import os
+import shutil
+import cPickle
+import datetime
+
+def main():
+	"""docstring for main"""
+
+	home = os.getenv("HOME")
+
+	os.chdir(home)
+
+	if os.path.exists(".bashwords/thewords.dat"):
+		print("Croiky! You've already got a dictionary.")
+		print("I don't want to overwrite your work, so I'll stop.")
+		exit()
+	else:
+		createDict()
+
+	writeToBash()
+               
+def writeToBash():
+	shutil.copy('.bash_profile', '.bash_profile.bak')
+
+	addendum = "# ------for bashwords-------" + \
+			   "\nalias define='python $HOME/.bashwords/bashwords.py define'" + \
+			   "\npython ~/.bashwords/bashwords.py cycle" + \
+			   "\nsource ~/.bashwords/exports.sh" + \
+			   "\n# --------------------------\n\n"
+
+	with open('.bash_profile', 'a') as bash:
+		bash.write(addendum)
+    
+	
+def createDict():
+	"""docstring for createDict"""
+	words = []
+	sample = {"word": "bucolic",
+			  "definition": "of or relating to the pleasant aspects" + \
+			  	  			" of the countryside or country life",
+			  "synonyms": ["tranquil", "rustic"],
+			  "hits": 0,
+			  "dateInserted": datetime.date.today()}
+
+	words.append(sample)
+
+	with open(".bashwords/thewords.dat", "w") as out:
+		cPickle.dump(words, out)
+		print("Dictionary created.")
+
+if __name__ == '__main__':
+	main()
+
