fd3133a5622f2bb2e943ebf05362437a62a94d8e
jamesob <james.obeirne@gmail.com>
2009-09-05 19:20:01 -0400

    separated cycle from bashwords.py for the sake of speed

---
+++ cycle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+from __future__ import with_statement
+import random
+import cPickle
+import os
+
+# cycle is separated from bashwords.py for the sake of speed;
+# since cycle is executed each time a terminal is opened, 
+# there's no need for all other functions to be loaded
+
+def cycle():
+	"""
+	Unpickles dictionary, selects a random word, loads relevant
+	information into BASH's environment.
+	"""
+	home = os.getenv("HOME")
+
+	with open(home + "/.bashwords/thewords.dat") as f:
+		dict = cPickle.load(f)
+
+	currWord = random.sample(dict, 1)[0]
+
+	with open(home + '/.bashwords/exports.sh', 'w') as f:
+		f.write('export currWord="%s"\n' % currWord["word"])
+		f.write('export currDefinition="%s"\n' % currWord["definition"])
+		f.write('export currSynonyms="%s"\n' % currWord["synonyms"])
+		f.write('PS1="$currWord|$PS1"\n')                            
+
+if __name__ == '__main__':
+	cycle()
