a2526172353aa433e77461c1bba5bf57480b9107
James O'Beirne <james.obeirne@gmail.com>
2009-11-12 18:37:13 -0500

    miscellaneous fixes all around

--- exports.sh
+++
@@ -1,4 +0,0 @@
-export currWord="vitiate"
-export currDefinition="corrupt morally or by intemperance of sensuality; to render ineffective"
-export currSynonyms="['debase', ' corrupt', ' mar', ' impair', ' invalidate']"
-PS1="$currWord|$PS1"
